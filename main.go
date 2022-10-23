9bba291cf5e826402b3c9c91c4ec8d98690415b5
Omar Kohl <omarkohl@posteo.net>
2022-10-23 19:53:06 +0200

    Make Go app serve the built React app
    The frontend was built with:
    
        cd website
        npm run build

--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"embed"
 	"fmt"
+	"io/fs"
 	"net/http"
 )
 
+//go:embed frontend/build
+var embeddedFiles embed.FS
+
 func main() {
-	http.HandleFunc("/", handler)
+	fmt.Println("Starting Server")
+	http.Handle("/", http.FileServer(getFileSystem()))
 	http.ListenAndServe(":8080", nil)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!")
+func getFileSystem() http.FileSystem {
+
+    // Get the build subdirectory as the
+    // root directory so that it can be passed
+    // to the http.FileServer
+	fsys, err := fs.Sub(embeddedFiles, "frontend/build")
+	if err != nil {
+		panic(err)
+	}
+
+	return http.FS(fsys)
 }
