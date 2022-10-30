5b47d43b83c12c719c907312f4fa50a377fb8ffd
Omar Kohl <omarkohl@posteo.net>
2022-10-30 18:52:29 +0100

    Add CORS support for API
    This is obviously only temporary and needs to be refined.

--- go.mod
+++ go.mod
@@ -4,13 +4,15 @@ go 1.19
 
 require (
 	github.com/99designs/gqlgen v0.17.20
+	github.com/go-chi/chi v1.5.4
+	github.com/gorilla/websocket v1.5.0
+	github.com/rs/cors v1.8.2
 	github.com/vektah/gqlparser/v2 v2.5.1
 )
 
 require (
 	github.com/agnivade/levenshtein v1.1.1 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
-	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/mitchellh/mapstructure v1.3.1 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
--- main.go
+++ main.go
@@ -5,9 +5,13 @@ import (
 	"net/http"
 
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/cleodora-forecasting/cleodora/graph"
 	"github.com/cleodora-forecasting/cleodora/graph/generated"
+	"github.com/go-chi/chi"
+	"github.com/gorilla/websocket"
+	"github.com/rs/cors"
 )
 
 var VERSION = "dev"
@@ -18,13 +22,34 @@ func main() {
 		VERSION,
 	)
 
+	router := chi.NewRouter()
+
+	// Add CORS middleware around every request
+	// See https://github.com/rs/cors for full option listing
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowCredentials: true,
+		Debug:            true,
+	}).Handler)
+
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
-	http.Handle("/playground/",
+	srv.AddTransport(&transport.Websocket{
+		Upgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				// Check against your desired domains here
+				return r.Host == "localhost:3000"
+			},
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+		},
+	})
+
+	router.Handle("/playground/",
 		playground.Handler("GraphQL playground", "/query"),
 	)
-	http.Handle("/query", srv)
+	router.Handle("/query", srv)
 
-	serveFrontend()
+	serveFrontend() // does this still work?
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", router)
 }
