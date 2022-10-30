ca6aea8d8a6e17f7ebed9d2bda674f985cb446d9
Omar Kohl <omarkohl@posteo.net>
2022-10-30 18:26:37 +0100

    Frontend GraphQL example with Apollo Client according to tutorial
    https://www.apollographql.com/docs/react/get-started

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+// Import everything needed to use the `useQuery` hook
+import { useQuery, gql } from '@apollo/client';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const GET_LOCATIONS = gql`
+  query GetLocations {
+    locations {
+      id
+      name
+      description
+      photo
+    }
+  }
+`;
+
+function DisplayLocations() {
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.locations.map(({ id, name, description, photo }) => (
+      <div key={id}>
+        <h3>{name}</h3>
+        <img width="400" height="250" alt="location-reference" src={`${photo}`} />
+        <br />
+        <b>About this location:</b>
+        <p>{description}</p>
+        <br />
+      </div>
+  ));
 }
 
-export default App;
+export default function App() {
+  return (
+      <div>
+        <h2>My first Apollo app 🚀</h2>
+        <br/>
+        <DisplayLocations />
+      </div>
+  );
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+const client = new ApolloClient({
+  uri: 'https://flyby-gateway.herokuapp.com/',
+  cache: new InMemoryCache(),
+});
+
+// Supported in React 18+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>,
+);
