9a5520515c740777972bc36a545bc535494ed37b
Omar Kohl <omarkohl@posteo.net>
2022-11-08 21:20:29 +0100

    Remove AllowedOrigins * for the backend due to browser error
    It's not allowed to have this in combination with AllowCredentials.
    
    The AllowOriginFunc already fulfills the purpose of allowing all
    origins.

--- main.go
+++ main.go
@@ -27,7 +27,6 @@ func main() {
 	// Add CORS middleware around every request
 	// See https://github.com/rs/cors for full option listing
 	router.Use(cors.New(cors.Options{
-		AllowedOrigins:   []string{"*"},
 		AllowedHeaders:   []string{"*"},
 		AllowCredentials: true,
 		Debug:            true,
