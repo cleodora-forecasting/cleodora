ab274766268431cc857dfc8a0ba3f8bc007ff8c6
Omar Kohl <omarkohl@posteo.net>
2022-11-10 19:32:34 +0100

    Rename config file for client to .cleoc.yml

--- client/cmd/root.go
+++ client/cmd/root.go
@@ -41,7 +41,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.client.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cleoc.yml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
@@ -58,10 +58,10 @@ func initConfig() {
 		home, err := os.UserHomeDir()
 		cobra.CheckErr(err)
 
-		// Search config in home directory with name ".client" (without extension).
+		// Search config in home directory with name ".cleoc" (without extension).
 		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".client")
+		viper.SetConfigType("yml")
+		viper.SetConfigName(".cleoc")
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
