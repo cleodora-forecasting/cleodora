7dc2a190831ba7d08bf4bdb5ae4e7cd502580886
Omar Kohl <omarkohl@posteo.net>
2022-11-12 18:03:07 +0100

    Client: Add 'url' parameter
    Also some minor cleanup.

--- client/cmd/root.go
+++ client/cmd/root.go
@@ -32,15 +32,19 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cleoc.yml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().StringVar(
+		&cfgFile,
+		"config",
+		"",
+		"config file (default is $HOME/.cleoc.yml)",
+	)
+	rootCmd.PersistentFlags().StringVarP(
+		&cfgFile,
+		"url",
+		"u",
+		"http://localhost:8080",
+		"base URL for the API",
+	)
 }
 
 // initConfig reads in config file and ENV variables if set.
