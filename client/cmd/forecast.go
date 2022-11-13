ab7152886c5caf30f21c29e1715a90c188d731db
Omar Kohl <omarkohl@posteo.net>
2022-11-13 18:05:43 +0100

    Client: Implement 'cleoc add forecast' (#32)

--- client/cmd/forecast.go
+++ client/cmd/forecast.go
@@ -1,37 +1,66 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"net/http"
+	"time"
 
+	"github.com/Khan/genqlient/graphql"
 	"github.com/spf13/cobra"
+
+	"github.com/cleodora-forecasting/cleodora/client/gqclient"
 )
 
 // forecastCmd represents the forecast command
 var forecastCmd = &cobra.Command{
-	Use:   "forecast",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Use:   "forecast SUMMARY RESOLUTION_DATE [DESCRIPTION]",
+	Short: "Add a new forecast",
+	Long: `Add a new forecast to Cleodora
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Calling %s\n", URL)
+SUMMARY is the title of the forecast.
+RESOLUTION_DATE needs to be in the format 2022-11-13T19:30:00+01:00
+DESCRIPTION is optional.
 
+It returns the ID of the forecast that was just created.
+
+Example:
+
+	cleoc add forecast "Will it rain tomorrow?" 2022-11-14T00:00:00+01:00 "If \
+		during the day it rains for more than 2 minutes at a time the \
+		forecast resolves as true."
+`,
+	Args: cobra.RangeArgs(2, 3),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		summary := args[0]
+		resolves, err := time.Parse(time.RFC3339, args[1])
+		if err != nil {
+			return err // todo wrap
+		}
+		description := ""
+		if len(args) == 3 {
+			description = args[2]
+		}
+		ctx := context.Background()
+		client := graphql.NewClient(
+			fmt.Sprintf("%s/query", URL),
+			http.DefaultClient,
+		)
+		f := gqclient.NewForecast{
+			Summary:     summary,
+			Description: description,
+			Resolves:    resolves,
+			Closes:      resolves, // should be optional
+		}
+		resp, err := gqclient.CreateForecast(ctx, client, f)
+		if err != nil {
+			return err // todo wrap
+		}
+		fmt.Println(resp.CreateForecast.Id)
+		return nil
 	},
 }
 
 func init() {
 	addCmd.AddCommand(forecastCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// forecastCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// forecastCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
