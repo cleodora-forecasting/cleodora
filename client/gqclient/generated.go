a90e1ab296f2b19f5ecf106284b33baefeb82bcf
Omar Kohl <omarkohl@posteo.net>
2022-11-13 17:49:51 +0100

    Client: Add 'createForecast' to gqclient

--- client/gqclient/generated.go
+++ client/gqclient/generated.go
@@ -9,6 +9,24 @@ import (
 	"github.com/Khan/genqlient/graphql"
 )
 
+// CreateForecastCreateForecast includes the requested fields of the GraphQL type Forecast.
+type CreateForecastCreateForecast struct {
+	Id string `json:"id"`
+}
+
+// GetId returns CreateForecastCreateForecast.Id, and is useful for accessing the field via an interface.
+func (v *CreateForecastCreateForecast) GetId() string { return v.Id }
+
+// CreateForecastResponse is returned by CreateForecast on success.
+type CreateForecastResponse struct {
+	CreateForecast CreateForecastCreateForecast `json:"createForecast"`
+}
+
+// GetCreateForecast returns CreateForecastResponse.CreateForecast, and is useful for accessing the field via an interface.
+func (v *CreateForecastResponse) GetCreateForecast() CreateForecastCreateForecast {
+	return v.CreateForecast
+}
+
 // GetForecastsForecastsForecast includes the requested fields of the GraphQL type Forecast.
 type GetForecastsForecastsForecast struct {
 	Id          string     `json:"id"`
@@ -49,6 +67,25 @@ type GetForecastsResponse struct {
 // GetForecasts returns GetForecastsResponse.Forecasts, and is useful for accessing the field via an interface.
 func (v *GetForecastsResponse) GetForecasts() []GetForecastsForecastsForecast { return v.Forecasts }
 
+type NewForecast struct {
+	Summary     string    `json:"summary"`
+	Description string    `json:"description"`
+	Resolves    time.Time `json:"resolves"`
+	Closes      time.Time `json:"closes"`
+}
+
+// GetSummary returns NewForecast.Summary, and is useful for accessing the field via an interface.
+func (v *NewForecast) GetSummary() string { return v.Summary }
+
+// GetDescription returns NewForecast.Description, and is useful for accessing the field via an interface.
+func (v *NewForecast) GetDescription() string { return v.Description }
+
+// GetResolves returns NewForecast.Resolves, and is useful for accessing the field via an interface.
+func (v *NewForecast) GetResolves() time.Time { return v.Resolves }
+
+// GetCloses returns NewForecast.Closes, and is useful for accessing the field via an interface.
+func (v *NewForecast) GetCloses() time.Time { return v.Closes }
+
 type Resolution string
 
 const (
@@ -57,6 +94,46 @@ const (
 	ResolutionNotApplicable Resolution = "NOT_APPLICABLE"
 	ResolutionUnresolved    Resolution = "UNRESOLVED"
 )
+
+// __CreateForecastInput is used internally by genqlient
+type __CreateForecastInput struct {
+	Input NewForecast `json:"input"`
+}
+
+// GetInput returns __CreateForecastInput.Input, and is useful for accessing the field via an interface.
+func (v *__CreateForecastInput) GetInput() NewForecast { return v.Input }
+
+func CreateForecast(
+	ctx context.Context,
+	client graphql.Client,
+	input NewForecast,
+) (*CreateForecastResponse, error) {
+	req := &graphql.Request{
+		OpName: "CreateForecast",
+		Query: `
+mutation CreateForecast ($input: NewForecast!) {
+	createForecast(input: $input) {
+		id
+	}
+}
+`,
+		Variables: &__CreateForecastInput{
+			Input: input,
+		},
+	}
+	var err error
+
+	var data CreateForecastResponse
+	resp := &graphql.Response{Data: &data}
+
+	err = client.MakeRequest(
+		ctx,
+		req,
+		resp,
+	)
+
+	return &data, err
+}
 
 func GetForecasts(
 	ctx context.Context,
