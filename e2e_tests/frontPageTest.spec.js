df6d601f145229d800cec1693c88391a0e1b0639
Omar Kohl <omarkohl@posteo.net>
2022-11-19 16:19:28 +0100

    Modify e2e test to create a new forecast with cleoc and verify that
    The test invokes the CLI tool cleoc to create a new forecast and the
    verifies that it actually appears on the page.
    
    Also make some minor improvements to the test, such as increasing the
    timeout.

--- e2e_tests/frontPageTest.spec.js
+++ e2e_tests/frontPageTest.spec.js
@@ -1,6 +1,7 @@
 const { By, Builder } = require('selenium-webdriver');
 const { suite } = require('selenium-webdriver/testing');
 const assert = require("assert");
+const child_process = require('child_process');
 
 const BASE_URL = 'http://localhost:8080';
 
@@ -10,22 +11,49 @@ suite(function(env) {
 
         before(async function() {
             driver = await new Builder().forBrowser('firefox').build();
+
+            // https://stackoverflow.com/a/32872753
+            let child = child_process.spawnSync(
+                "../build/cleoc",
+                [
+                    'add',
+                    'forecast',
+                    'Is this a test forecast?',
+                    '2022-12-01T15:00:00+01:00'
+                ],
+                { encoding : 'utf8' },
+            );
+            console.log("'cleoc add forecast' finished.");
+            if(child.error) {
+                console.log("ERROR: ",child.error);
+            }
+            console.log("stdout: ",child.stdout);
+            console.log("stderr: ",child.stderr);
+            console.log("exit code: ",child.status);
         });
 
         after(async () => await driver.quit());
 
-        it('Verify title and header', async function() {
+        it('Verify title, header and new prediction appears', async function() {
+            await driver.manage().setTimeouts({ implicit: 2000 });
+
             await driver.get(BASE_URL);
 
             let title = await driver.getTitle();
-            assert.equal("Cleodora", title);
 
-            await driver.manage().setTimeouts({ implicit: 500 });
+            assert.equal("Cleodora", title, "Title does not match");
 
             let header = await driver.findElement(By.css('h2'));
 
             let value = await header.getText();
-            assert.equal("Cleodora", value);
+            assert.equal("Cleodora", value, "Header does not match");
+
+            let bodyText = await driver.findElement(By.tagName('body')).getText();
+            console.log(bodyText);
+            assert(
+                bodyText.includes("Is this a test forecast?"),
+                "Prediction created with cleoc not on the page",
+            );
         });
 
     });
