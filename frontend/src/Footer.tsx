9403994f5d6e8821f89a3a7333eb3f7e8f2d3fd1
Omar Kohl <omarkohl@posteo.net>
2022-12-23 14:17:55 +0100

    GUI: Add link to cleodora.org to footer
    The empty Box on the right of the footer is for including a text later.
    In pratice it's probably only relevant for the demo.

--- frontend/src/Footer.tsx
+++ frontend/src/Footer.tsx
@@ -2,6 +2,7 @@ import {FC} from "react";
 import {gql} from "./__generated__"
 import "./Footer.css"
 import {useQuery} from "@apollo/client";
+import {Box, Grid} from "@mui/material";
 
 export const GET_METADATA = gql(`
     query GetMetadata {
@@ -15,6 +16,14 @@ export const Footer: FC = () => {
     const {data} = useQuery(GET_METADATA);
 
     return <footer>
-        Version: {data?.metadata.version}
+        <Grid container spacing={2}>
+            <Grid item lg={6}>
+                Cleodora Forecasting (<a href="https://cleodora.org">cleodora.org</a>). Version: {data?.metadata.version}
+            </Grid>
+            <Grid item lg={6}>
+                <Box display="flex" justifyContent="flex-end">
+                </Box>
+            </Grid>
+        </Grid>
     </footer>
 }
