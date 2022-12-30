26e7c01eaeb67d1c1a5f64ac1dd6efa76f1f9910
Omar Kohl <omarkohl@posteo.net>
2022-12-30 19:03:48 +0100

    cleoc: Improve help text when invoking 'cleoc add'
    By removing the RunE function the usage text is displayed automatically.

--- cleoc/cmd/add.go
+++ cleoc/cmd/add.go
@@ -9,16 +9,9 @@ import (
 func buildAddCommand(app *cleoc.App) *cobra.Command {
 	var addCmd = &cobra.Command{
 		Use:   "add",
-		Short: "A brief description of your command",
-		Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return cmd.Help()
-		},
+		Short: "Add new things to Cleodora",
+		Long: `Add new things to Cleodora, such as forecasts
+`,
 	}
 
 	addCmd.AddCommand(buildAddForecastCommand(app))
