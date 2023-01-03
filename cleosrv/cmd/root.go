e4a56771ebe47fc2124ae8ee7ca07f0b28ef721f
Omar Kohl <omarkohl@posteo.net>
2023-01-03 20:20:13 +0100

    cleosrv: Allow running on Windows via double click (#124)

--- cleosrv/cmd/root.go
+++ cleosrv/cmd/root.go
@@ -43,6 +43,7 @@ Visit https://cleodora.org for more information.
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	cobra.MousetrapHelpText = ""
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
