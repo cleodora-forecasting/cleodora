7a15f143f1a40232c455ae8bc9dc57ec0cda952e
Omar Kohl <omarkohl@posteo.net>
2023-01-04 21:33:35 +0100

    Frontend: Fix 'App' test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {ApolloProvider} from "@apollo/client";
+import {client} from "./client";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('complete overview is rendered', async () => {
+    render(
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    );
+
+    expect(screen.getByText("Cleodora")).toBeInTheDocument();
+    expect(screen.getByText("cleodora.org")).toBeInTheDocument();
+    expect(await screen.findByRole("heading", {name: "Forecasts"})).toBeInTheDocument();
+    expect(await screen.findByRole("heading", {name: "Add Forecast"})).toBeInTheDocument();
 });
--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -16,6 +16,52 @@ export const handlers: RequestHandler[] = [
             }),
         )
     }),
+    graphql.query('GetForecasts', (req, res, ctx) => {
+       return res(
+           ctx.data({
+               "forecasts":[
+                   {
+                       "id":"1",
+                       "title":"Will \"The Fabelmans\" win \"Best Picture\" at the Oscars 2023?",
+                       "description":"",
+                       "created":"2022-10-30T17:05:00+01:00",
+                       "closes":null,
+                       "resolves":"2023-03-11T23:59:00+01:00",
+                       "resolution":"UNRESOLVED",
+                       "estimates":[
+                           {
+                               "id":"1",
+                               "probabilities":[
+                                   {
+                                       "id":"1",
+                                       "value":30,
+                                       "outcome":{
+                                           "id":"1",
+                                           "text":"Yes",
+                                           "__typename":"Outcome"
+                                       },
+                                       "__typename":"Probability"
+                                   },
+                                   {
+                                       "id":"2",
+                                       "value":70,
+                                       "outcome":{
+                                           "id":"2",
+                                           "text":"No",
+                                           "__typename":"Outcome"
+                                       },
+                                       "__typename":"Probability"
+                                   }
+                                   ],
+                               "__typename":"Estimate"
+                           }
+                           ]
+                       ,"__typename":"Forecast"
+                   }
+               ]
+           }),
+       )
+    }),
     rest.get('/config.json', (req, res, ctx) => {
         console.log("config.json called");
         return res(ctx.json({ FOOTER_TEXT: "Footer text for a test" }))
