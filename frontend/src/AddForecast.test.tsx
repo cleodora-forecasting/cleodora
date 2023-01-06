02b9a456b8dbdf93e61f8249c5087d7a11015de4
Omar Kohl <omarkohl@posteo.net>
2023-01-06 13:08:12 +0100

    Frontend: Modify AddForecast to also allow setting estimate (#87)
    Also several other minor improvements.

--- frontend/src/AddForecast.test.tsx
+++ frontend/src/AddForecast.test.tsx
@@ -8,7 +8,21 @@ import {graphql} from "msw";
 
 test('after adding a forecast a success msg is shown', async () => {
     const user = userEvent.setup()
-    let requestBody;
+    let requestBody: {
+        variables: {
+            forecast:
+                {
+                    title: string;
+                    resolves: string;
+                    closes: string;
+                    description: string;
+                };
+            estimate: {
+                reason: string;
+                probabilities: {value: number; outcome: {text: string}; }[];
+            };
+        };
+    } | undefined;
 
     server.use(
         graphql.mutation("createForecast", async (req, res, ctx) => {
@@ -34,19 +48,43 @@ test('after adding a forecast a success msg is shown', async () => {
     const expectedTitle = "Will this test pass?";
     const inputResolves = "01/13/2023 10:00 AM";
     const expectedResolves = "2023-01-13T09:00:00.000Z";
+    const expectedReason = "It was written carefully and is not complicated.";
 
     await user.type(screen.getByLabelText('Title'), expectedTitle);
     await user.clear(screen.getByLabelText('Closes'));
     await user.clear(screen.getByLabelText('Resolves'));
     await user.type(screen.getByLabelText('Resolves'), inputResolves);
+    await user.type(screen.getByLabelText('Reason'), expectedReason);
+    await user.type(await screen.findByLabelText('Outcome0'), 'Yes');
+    await user.type(await screen.findByLabelText('Probability0'), '95');
+    await user.click(await screen.findByLabelText('add probability'));
+    await user.type(await screen.findByLabelText('Outcome1'), 'No');
+    await user.type(await screen.findByLabelText('Probability1'), '5');
     await user.click(await screen.findByRole("button", {name: "Add Forecast"}));
 
     expect(await screen.findByText('Saved "Mock title" with ID 999.')).toBeInTheDocument();
     expect(requestBody).toBeTruthy();
-    if (requestBody) {
-        expect(requestBody!.variables.forecast.title).toBe(expectedTitle);
-        expect(requestBody!.variables.forecast.resolves).toBe(expectedResolves);
-        expect(requestBody!.variables.forecast.closes).toBeNull();
-        expect(requestBody!.variables.forecast.description).toBe("");
+    if (!requestBody) {
+        return
     }
-});
+    expect(requestBody.variables.forecast.title).toBe(expectedTitle);
+
+    expect(requestBody.variables.forecast.resolves).toBe(expectedResolves);
+    expect(requestBody.variables.forecast.closes).toBeNull();
+    expect(requestBody.variables.forecast.description).toBe("");
+
+    // Probability estimate
+    expect(requestBody.variables.estimate.reason).toBe(expectedReason);
+    expect(requestBody.variables.estimate.probabilities).toHaveLength(2);
+    let probYes = requestBody.variables.estimate.probabilities[0];
+    let probNo = requestBody.variables.estimate.probabilities[1];
+    if (probYes.value !== 95) {
+        // switch around, since we shouldn't assume order
+        probYes = requestBody.variables.estimate.probabilities[1];
+        probNo = requestBody.variables.estimate.probabilities[0];
+    }
+    expect(probYes.value).toBe(95);
+    expect(probYes.outcome.text).toBe('Yes');
+    expect(probNo.value).toBe(5);
+    expect(probNo.outcome.text).toBe('No');
+}, 15000);
--- frontend/src/AddForecast.tsx
+++ frontend/src/AddForecast.tsx
@@ -2,14 +2,16 @@ import React, {FC, useState} from "react";
 import {useMutation} from "@apollo/client";
 import {GET_FORECASTS} from "./ForecastList";
 import {gql} from "./__generated__"
+import {NewProbability} from "./__generated__/graphql";
 import {
     CreateForecastMutation,
     CreateForecastMutationVariables,
 } from "./__generated__/graphql";
-import {TextField, Button, Grid} from "@mui/material";
+import {TextField, Button, Grid, IconButton} from "@mui/material";
 import {DateTimePicker, LocalizationProvider} from "@mui/x-date-pickers";
 import dayjs, {Dayjs} from "dayjs";
 import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
 const ADD_FORECAST = gql(`
     mutation createForecast($forecast: NewForecast!, $estimate: NewEstimate!) {
@@ -23,8 +25,10 @@ const ADD_FORECAST = gql(`
 export const AddForecast: FC = () => {
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
-    const [closes, setCloses] = React.useState<Dayjs | null>(dayjs());
-    const [resolves, setResolves] = React.useState<Dayjs | null>(dayjs());
+    const [closes, setCloses] = React.useState<Dayjs | null>(null);
+    const [resolves, setResolves] = useState(dayjs());
+    const [reason, setReason] = useState('');
+    const [probabilities, setProbabilities] = useState([{outcome: {text: ''}, value: 0} as NewProbability]);
 
 //    useMutation<TableSizeMutation, TableSizeMutationVariables>
     const [addForecast, {error, data}] = useMutation<CreateForecastMutation, CreateForecastMutationVariables>(ADD_FORECAST, {
@@ -42,25 +46,27 @@ export const AddForecast: FC = () => {
                 resolves,
             },
             estimate: {
-                reason: "TODO Just a gut feeling I have.",
-                probabilities: [
-                    {
-                        value: 50,
-                        outcome: {
-                            text: "TODO Yes",
-                        },
-                    },
-                    {
-                        value: 50,
-                        outcome: {
-                            text: "TODO No",
-                        },
-                    },
-                ],
+                reason,
+                probabilities
             }
         },
     });
 
+    // https://beta.reactjs.org/learn/updating-arrays-in-state#replacing-items-in-an-array
+    function handleModifyProbability(index: number, outcome: string, value: number) {
+        const nextProbabilities = probabilities.map((c, i) => {
+            if (i === index) {
+                // Increment the clicked counter
+                return {outcome: {text: outcome}, value: value};
+            } else {
+                // The rest haven't changed
+                return c;
+            }
+        });
+        setProbabilities(nextProbabilities);
+    }
+
+    // https://rajputankit22.medium.com/add-dynamically-textfields-in-react-js-71320aee9a8d
     return (
         <div>
             <h3>Add Forecast</h3>
@@ -78,12 +84,14 @@ export const AddForecast: FC = () => {
                     addForecast().then(() => {
                         setTitle('');
                         setDescription('');
-                        setCloses(dayjs());
+                        setCloses(null);
                         setResolves(dayjs());
+                        setReason('');
+                        setProbabilities([{value: 0, outcome: {text: ''}}]);
                     }).catch(reason => (console.log("error addForecast()", reason)));
                 }}>
                     <Grid container direction="row" alignItems="flex-start" spacing={1} justifyItems="flex-start">
-                        <Grid container spacing={2}>
+                        <Grid container spacing={3}>
                             <Grid item xs={12}>
                                 <TextField
                                     label="Title"
@@ -108,17 +116,71 @@ export const AddForecast: FC = () => {
                                     onChange={(newValue: Dayjs | null) => {
                                         setCloses(newValue);
                                     }}
-                                    renderInput={(params) => <TextField {...params} />}
+                                    renderInput={(params) => (
+                                        <TextField
+                                            {...params}
+                                            helperText="(optional) No probability updates after this date."
+                                        />
+                                    )}
                                 />
                             </Grid>
                             <Grid item xs={12}>
                                 <DateTimePicker
                                     label="Resolves"
                                     value={resolves}
-                                    onChange={(newValue: Dayjs | null) => {
+                                    onChange={(newValue) => {
+                                        if (!newValue) {
+                                            return
+                                        }
                                         setResolves(newValue);
                                     }}
                                     renderInput={(params) => <TextField {...params} />}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                            {probabilities.map((prob, index) => (
+                                <Grid container key={index}>
+                                    <Grid item xs={2}>
+                                        <TextField
+                                            value={prob.outcome.text}
+                                            onChange={e => handleModifyProbability(index, e.target.value, prob.value)}
+                                            label={`Outcome${index}`}
+                                            variant="filled"/>
+                                    </Grid>
+                                    <Grid item xs={2}>
+                                        <TextField
+                                            value={prob.value}
+                                            inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+                                            onChange={
+                                                e => {
+                                                    if (isNaN(Number(e.target.value))) {
+                                                        return
+                                                    }
+                                                    handleModifyProbability(index, prob.outcome.text, Number(e.target.value))
+                                                }
+                                            }
+                                            label={`Probability${index}`}
+                                            variant="filled"/>
+                                    </Grid>
+                                    <Grid item xs={1}>
+                                        <IconButton
+                                            aria-label="add probability"
+                                            onClick={_ => setProbabilities(old => [...old, {outcome: {text: ''}, value: 0}])}
+                                        >
+                                            <AddCircleOutlineIcon />
+                                        </IconButton>
+                                    </Grid>
+                                </Grid>
+                            ))}
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    value={reason}
+                                    onChange={e => setReason(e.target.value)}
+                                    label="Reason"
+                                    multiline
+                                    variant="filled"
+                                    helperText="Why these probabilities?"
                                 />
                             </Grid>
                             <Grid item xs={12}>
