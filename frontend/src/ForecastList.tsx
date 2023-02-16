6238c2a36312d58a92a5eb3c33e0f1db5ea56082
Omar Kohl <omarkohl@posteo.net>
2023-02-16 20:02:37 +0100

    feat(frontend): improve list display e.g. by showing errors (#173)
    Previously if there was an error only the message "Error :-(" was
    displayed.
    
    Now the table of forecasts will be shown (empty though) and an error
    message at the top, if there is an error.
    
    The table body was moved into a variable, but without changing anything
    else about it.

--- frontend/src/ForecastList.tsx
+++ frontend/src/ForecastList.tsx
@@ -1,4 +1,4 @@
-import {FC} from "react";
+import React, {FC} from "react";
 import {useQuery} from "@apollo/client";
 import {gql} from "./__generated__"
 import {Paper, Table,
@@ -30,15 +30,79 @@ export const GET_FORECASTS = gql(`
 `);
 
 export const ForecastList: FC = () => {
-    const {loading, error, data} = useQuery(GET_FORECASTS);
+    const {error, data} = useQuery(GET_FORECASTS);
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error :-(</p>;
-    if (data === undefined) return <p>Error :-(</p>
+    let errorBox:JSX.Element;
+    if (error) {
+        const errMessages = new Array(error.message);
+        if (error.message == 'NetworkError when attempting to fetch' +
+            ' resource.') {
+            errMessages.push('Make sure the API is running and reachable.');
+        }
+        errorBox =
+            <p style={{color:"red"}}>
+                <strong>{error.name}: </strong>
+                {errMessages.join(' ')}
+            </p>
+    } else {
+        errorBox = <></>
+    }
+
+    let tableBody:JSX.Element;
+    if (data === undefined) {
+        tableBody = <></>;
+    } else {
+        tableBody = <>
+            {
+                data.forecasts.map(
+                    f => {
+                        let estimates = ""
+                        if (Array.isArray(f.estimates)) {
+                            // TODO the next line depends much
+                            //  on what the API returns. It
+                            //  probably needs to be adjusted.
+                            const lastEstimate = f.estimates[f.estimates.length - 1];
+                            if (lastEstimate != null) {
+                                estimates = lastEstimate.probabilities.map(
+                                    p => {
+                                        if (p != null) {
+                                            return p.outcome.text + ": " + p.value.toString() + "%"
+                                        }
+                                        return ""
+                                    }
+                                ).join(" | ")
+                            }
+                        }
+                        let closes = ""
+                        if (f.closes) {
+                            closes = new Date(f.closes as string).toLocaleString()
+                        }
+                        return (
+                            <TableRow
+                                key={f.id}
+                                sx={{'&:last-child td, &:last-child th': {border: 0}}}
+                            >
+                                <TableCell component="th"
+                                           scope="row">{f.title}</TableCell>
+                                <TableCell
+                                    align="right">{new Date(f.created as string).toLocaleString()}</TableCell>
+                                <TableCell align="right">{closes}</TableCell>
+                                <TableCell
+                                    align="right">{new Date(f.resolves as string).toLocaleString()}</TableCell>
+                                <TableCell
+                                    align="right">{f.resolution}</TableCell>
+                                <TableCell align="right">{estimates}</TableCell>
+                            </TableRow>
+                        )
+                    }
+                )
+            }
+        </>
+    }
     return (
         <div>
             <Typography variant="h5">Forecasts</Typography>
-
+            {errorBox}
             <TableContainer component={Paper}>
                 <Table sx={{ minWidth: 650 }} aria-label="forecasts">
                     <TableHead>
@@ -52,46 +116,7 @@ export const ForecastList: FC = () => {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {
-                            data.forecasts.map(
-                                f => {
-                                    let estimates = ""
-                                    if (Array.isArray(f.estimates)) {
-                                        // TODO the next line depends much
-                                        //  on what the API returns. It
-                                        //  probably needs to be adjusted.
-                                        const lastEstimate = f.estimates[f.estimates.length - 1];
-                                        if (lastEstimate != null) {
-                                            estimates = lastEstimate.probabilities.map(
-                                                p => {
-                                                    if (p != null) {
-                                                        return p.outcome.text + ": " + p.value.toString() + "%"
-                                                    }
-                                                    return ""
-                                                }
-                                            ).join(" | ")
-                                        }
-                                    }
-                                    let closes = ""
-                                    if (f.closes) {
-                                        closes = new Date(f.closes as string).toLocaleString()
-                                    }
-                                    return (
-                                        <TableRow
-                                            key={f.id}
-                                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                        >
-                                            <TableCell component="th" scope="row">{f.title}</TableCell>
-                                            <TableCell align="right">{new Date(f.created as string).toLocaleString()}</TableCell>
-                                            <TableCell align="right">{closes}</TableCell>
-                                            <TableCell align="right">{new Date(f.resolves as string).toLocaleString()}</TableCell>
-                                            <TableCell align="right">{f.resolution}</TableCell>
-                                            <TableCell align="right">{estimates}</TableCell>
-                                        </TableRow>
-                                    )
-                                }
-                            )
-                        }
+                        {tableBody}
                     </TableBody>
                 </Table>
             </TableContainer>
