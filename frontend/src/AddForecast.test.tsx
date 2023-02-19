6a13b1190c314af35f950906d3dd6c940ec2292a
Omar Kohl <omarkohl@posteo.net>
2023-02-19 13:25:50 +0100

    test(frontend): add test to add forecast by 'tabbing' through the form

--- frontend/src/AddForecast.test.tsx
+++ frontend/src/AddForecast.test.tsx
@@ -92,3 +92,87 @@ test('after adding a forecast a success msg is shown', async () => {
         expectedProbabilities.delete(p.outcome.text);
     });
 }, 15000);
+
+
+test('add a forecast by tabbing with the keyboard', async () => {
+    const user = userEvent.setup();
+    let requestBody: AddForecastRequest | undefined;
+
+    server.use(
+        graphql.mutation("createForecast", async (req, res, ctx) => {
+            requestBody = await req.json();
+            return res(
+                ctx.data({
+                    "createForecast": {
+                        "id": "999",
+                        "title": "Mock title",
+                        "__typename": "Forecast"
+                    }
+                }),
+            )
+        }),
+    )
+
+    render(
+        <ApolloProvider client={client}>
+            <AddForecast />
+        </ApolloProvider>
+    );
+
+    const expectedTitle = "Will this test pass?";
+    const inputResolves = "01/13/2023 10:00 AM";
+    const expectedResolves = "2023-01-13T10:00:00.000Z";
+    const expectedReason = "It was written carefully and is not complicated.";
+
+    await user.type(screen.getByLabelText('Title *'), expectedTitle);
+    await user.tab(); // to description
+    await user.tab(); // to resolves
+    await user.keyboard('{Delete}'.repeat(20) + inputResolves);
+    await user.tab(); // to calendar icon
+    await user.tab(); // to 1. Outcome
+    await user.keyboard("Yes");
+    await user.tab();
+    await user.keyboard("95");
+    await user.tab();
+    await user.tab();
+    await user.keyboard("No");
+    await user.tab();
+    await user.keyboard("4");
+    await user.tab();
+    await user.tab();
+    await user.keyboard('{Enter}')
+    await user.keyboard("Maybe");
+    await user.tab();
+    await user.keyboard("1");
+    await user.tab();
+    await user.tab();
+    await user.tab();
+    await user.keyboard(expectedReason);
+    await user.tab();
+    await user.keyboard('{Enter}');
+
+    expect(await screen.findByText('Saved "Mock title" with ID 999.')).toBeInTheDocument();
+
+    // Verify the GraphQL mutation contains the data we expect
+    expect(requestBody).toBeTruthy();
+    if (!requestBody) {
+        return
+    }
+    expect(requestBody.variables.forecast.title).toBe(expectedTitle);
+    expect(requestBody.variables.forecast.resolves).toBe(expectedResolves);
+    expect(requestBody.variables.forecast.description).toBe("");
+
+    // Probability estimate
+    expect(requestBody.variables.estimate.reason).toBe(expectedReason);
+    expect(requestBody.variables.estimate.probabilities).toHaveLength(3);
+    const expectedProbabilities = new Map<string, number>([
+        ['Yes', 95],
+        ['No', 4],
+        ['Maybe', 1],
+    ]);
+    requestBody.variables.estimate.probabilities.forEach(p => {
+        expect(expectedProbabilities.get(p.outcome.text)).toBe(p.value);
+        // ensure every outcome only appears once
+        expectedProbabilities.delete(p.outcome.text);
+    });
+}, 15000);
