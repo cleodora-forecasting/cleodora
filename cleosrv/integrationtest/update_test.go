5832648f7053926f7f4dccead735caa779dcb2b0
Omar Kohl <omarkohl@posteo.net>
2023-02-24 17:12:29 +0100

    test(cleosrv): add first update test and 0.1.1 test.db
    The DB was created with the create_test_sql script.

---
+++ cleosrv/integrationtest/update_test.go
@@ -0,0 +1,25 @@
+package integrationtest
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestUpdate_From_0_1_1(t *testing.T) {
+	tDir, err := os.MkdirTemp("", t.Name()+"_")
+	require.NoError(t, err)
+	dbSrc := filepath.Join("testdata", t.Name(), "test.db")
+	dbPath := filepath.Join(tDir, "test.db")
+	t.Log("executing with DB", dbPath)
+	err = CopyFile(dbSrc, dbPath)
+	require.NoError(t, err)
+
+	c := initServerAndGetClient(t, dbPath)
+	resp, err := GetForecasts(context.Background(), c)
+	require.NoError(t, err)
+	require.Len(t, resp.Forecasts, 2)
+}
