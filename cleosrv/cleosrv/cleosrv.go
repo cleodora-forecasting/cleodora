c0af30cf83834feeac819066f032c1417ab0d502
Omar Kohl <omarkohl@posteo.net>
2023-02-25 20:28:18 +0100

    feat(cleosrv): implement DB migrations and migrate 0.1.1 to 0.2.0 (#264)

--- cleosrv/cleosrv/cleosrv.go
+++ cleosrv/cleosrv/cleosrv.go
@@ -101,15 +101,220 @@ func (a *App) InitDB() (*gorm.DB, error) {
 		return nil, err
 	}
 
-	err = db.AutoMigrate(
+	err = migrateDB(db)
+	if err != nil {
+		return nil, fmt.Errorf("migrating data: %w", err)
+	}
+
+	return db, nil
+}
+
+// migrateDB creates the DB tables and migrates the schema and data to the
+// current version if necessary.
+func migrateDB(db *gorm.DB) error {
+	type Migrations struct {
+		ID      string `gorm:"uniqueIndex"`
+		Applied time.Time
+	}
+	dbIsEmpty := !db.Migrator().HasTable("forecasts")
+	err := db.Transaction(func(tx *gorm.DB) error {
+		return tx.Migrator().AutoMigrate(&Migrations{})
+	})
+	if err != nil {
+		return fmt.Errorf("auto migrating 'migrations' table: %w", err)
+	}
+	if dbIsEmpty {
+		err := db.Transaction(func(tx *gorm.DB) error {
+			err := createDb(tx)
+			if err != nil {
+				return fmt.Errorf("creating tables: %w", err)
+			}
+			// save all migrations as done in the DB, without executing the
+			// functions.
+			var mEntries []Migrations
+			for _, m := range dbMigrations {
+				mEntries = append(
+					mEntries,
+					Migrations{
+						ID:      m.ID,
+						Applied: time.Now().UTC(),
+					},
+				)
+			}
+			ret := tx.Create(mEntries)
+			if ret.Error != nil {
+				return fmt.Errorf("storing migrations as done in DB: %w", ret.Error)
+			}
+			return nil
+		})
+		return err
+	}
+	for _, m := range dbMigrations {
+		var count int64
+		ret := db.Model(&Migrations{}).Where("id = ?", m.ID).Count(&count)
+		if ret.Error != nil {
+			return fmt.Errorf("selecting migration %v: %w", m.ID, ret.Error)
+		}
+		if count == 1 {
+			continue // migration already ran in the past
+		}
+		err := db.Transaction(func(tx *gorm.DB) error {
+			fmt.Printf("Running DB migration '%v'\n", m.ID)
+			if m.Up != nil {
+				err = m.Up(tx)
+				if err != nil {
+					return fmt.Errorf("running %v: %w", m.ID, err)
+				}
+			}
+			ret = tx.Create(Migrations{
+				ID:      m.ID,
+				Applied: time.Now().UTC(),
+			})
+			if ret.Error != nil {
+				return fmt.Errorf("saving migration %v: %w", m.ID, ret.Error)
+			}
+			fmt.Printf("Finished DB migration '%v'\n", m.ID)
+			return nil
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func createDb(db *gorm.DB) error {
+	return db.AutoMigrate(
 		&dbmodel.Forecast{},
 		&dbmodel.Outcome{},
 		&dbmodel.Probability{},
 		&dbmodel.Estimate{},
 	)
-	if err != nil {
-		return db, err
-	}
+}
 
-	return db, nil
+type dbMigration struct {
+	ID string
+	Up func(db *gorm.DB) error
+}
+
+// dbMigrations is a list of all the migrations that need to be applied to an
+// old DB to update to the current version. They are run in order. Append new
+// ones to the end.
+var dbMigrations = []dbMigration{
+	{
+		ID: "0.1.0 init",
+		Up: nil,
+	},
+	{
+		ID: "0.2.0 convert forecast dates",
+		Up: func(db *gorm.DB) error {
+			type Forecast struct {
+				gorm.Model
+				Title       string
+				Description string
+				Created     time.Time
+				Resolves    time.Time
+				Closes      *time.Time
+			}
+			var forecasts []Forecast
+			ret := db.Find(&forecasts)
+			if ret.Error != nil {
+				return fmt.Errorf("getting forecasts: %w", ret.Error)
+			}
+			for _, f := range forecasts {
+				f.Created = f.Created.UTC()
+				f.Resolves = f.Resolves.UTC()
+				f.CreatedAt = f.CreatedAt.UTC()
+				if f.Closes != nil && f.Closes.IsZero() {
+					f.Closes = nil
+				} else if f.Closes != nil {
+					temp := f.Closes.UTC()
+					f.Closes = &temp
+				}
+				if f.Resolves.Before(f.Created) {
+					f.Resolves = f.Created
+				}
+				if f.Closes != nil {
+					if f.Closes.Before(f.Created) {
+						newCloses := f.Created
+						f.Closes = &newCloses
+					}
+					if f.Resolves.Before(*f.Closes) {
+						newCloses := f.Resolves
+						f.Closes = &newCloses
+					}
+				}
+				ret = db.Save(f)
+				if ret.Error != nil {
+					return fmt.Errorf("saving %v: %w", f.ID, ret.Error)
+				}
+			}
+			return nil
+		},
+	},
+	{
+		ID: "0.2.0 convert estimate dates to UTC",
+		Up: func(db *gorm.DB) error {
+			type Estimate struct {
+				gorm.Model
+				Created time.Time
+			}
+			var estimates []Estimate
+			ret := db.Find(&estimates)
+			if ret.Error != nil {
+				return fmt.Errorf("getting estimates: %w", ret.Error)
+			}
+			for _, e := range estimates {
+				e.Created = e.Created.UTC()
+				e.CreatedAt = e.CreatedAt.UTC()
+				ret = db.Save(e)
+				if ret.Error != nil {
+					return fmt.Errorf("saving %v: %w", e.ID, ret.Error)
+				}
+			}
+			return nil
+		},
+	},
+	{
+		ID: "0.2.0 convert outcome dates to UTC",
+		Up: func(db *gorm.DB) error {
+			type Outcome struct {
+				gorm.Model
+			}
+			var outcomes []Outcome
+			ret := db.Find(&outcomes)
+			if ret.Error != nil {
+				return fmt.Errorf("getting outcomes: %w", ret.Error)
+			}
+			for _, o := range outcomes {
+				o.CreatedAt = o.CreatedAt.UTC()
+				ret = db.Save(o)
+				if ret.Error != nil {
+					return fmt.Errorf("saving %v: %w", o.ID, ret.Error)
+				}
+			}
+			return nil
+		},
+	},
+	{
+		ID: "0.2.0 convert probability dates to UTC",
+		Up: func(db *gorm.DB) error {
+			type Probability struct {
+				gorm.Model
+			}
+			var probabilities []Probability
+			ret := db.Find(&probabilities)
+			if ret.Error != nil {
+				return fmt.Errorf("getting probabilities: %w", ret.Error)
+			}
+			for _, p := range probabilities {
+				p.CreatedAt = p.CreatedAt.UTC()
+				ret = db.Save(p)
+				if ret.Error != nil {
+					return fmt.Errorf("saving %v: %w", p.ID, ret.Error)
+				}
+			}
+			return nil
+		},
+	},
 }
--- cleosrv/integrationtest/update_test.go
+++ cleosrv/integrationtest/update_test.go
@@ -1,18 +1,138 @@
 package integrationtest
 
 import (
+	"bytes"
 	"context"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"regexp"
 	"testing"
+	"time"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestUpdate_From_0_1_1(t *testing.T) {
 	tDir, err := os.MkdirTemp("", t.Name()+"_")
 	require.NoError(t, err)
-	dbSrc := filepath.Join("testdata", t.Name(), "test.db")
+	dbSrc := filepath.Join("testdata", "test_0.1.1.db")
+	dbPath := filepath.Join(tDir, "test.db")
+	t.Log("executing with DB", dbPath)
+	err = CopyFile(dbSrc, dbPath)
+	require.NoError(t, err)
+
+	c := initServerAndGetClient(t, dbPath)
+	respGetForecasts, err := GetForecasts(context.Background(), c)
+	require.NoError(t, err)
+
+	expectedTitles := []string{
+		"Forecast with closes set to Go time null value and 3 outcomes (0.1.1)",
+		"Forecast with illogical created/resolves/closes (0.1.1)",
+		"Forecast with closes after resolves (0.1.1)",
+		"Just a regular forecast (0.1.1)",
+	}
+	var foundTitles []string
+	for _, f := range respGetForecasts.Forecasts {
+		foundTitles = append(foundTitles, f.Title)
+		t.Log(f)
+		assertUTC(t, f.Created)
+		assertUTC(t, f.Resolves)
+		if f.Closes != nil {
+			assertUTC(t, *f.Closes)
+		}
+		for _, e := range f.Estimates {
+			assertUTC(t, e.Created)
+		}
+
+		if f.Title == "Forecast with closes set to Go time null value and 3 outcomes (0.1.1)" {
+			// In the 0.1.1 DB closes is set to 0001-01-01 00:00:00+00:00 i.e.
+			// the Go time.Time null value
+			assert.Nil(t, f.Closes)
+		}
+		if f.Title == "Forecast with illogical created/resolves/closes (0.1.1)" {
+			assert.Equal(t, f.Created, f.Resolves)
+			assert.Equal(t, f.Created, *f.Closes)
+		}
+		if f.Title == "Forecast with closes after resolves (0.1.1)" {
+			assert.Equal(t, f.Resolves, *f.Closes)
+		}
+		if f.Title == "Just a regular forecast (0.1.1)" {
+			// Verify some values that we know are contained in the DB to
+			// ensure nothing got lost. If the DB is re-generated, this part of
+			// the test probably has to be updated.
+			expectedCreated, err := time.Parse(
+				time.RFC3339,
+				"2023-02-25T09:49:59.276264803Z",
+			)
+			assert.Nil(t, err)
+			assert.Equal(t, expectedCreated, f.Created)
+			expectedResolves, err := time.Parse(
+				time.RFC3339,
+				"2023-03-27T09:49:59Z",
+			)
+			assert.Nil(t, err)
+			assert.Equal(t, expectedResolves, f.Resolves)
+			assert.Nil(t, f.Closes)
+		}
+	}
+	assert.ElementsMatch(t, expectedTitles, foundTitles)
+
+	// Create a new forecast to verify it's possible after an update
+
+	newForecast := NewForecast{
+		Title: "Will it rain tomorrow?",
+		Description: "It counts as rain if between 9am and 9pm there are " +
+			"30 min or more of uninterrupted precipitation.",
+		Closes:   timePointer(time.Now().Add(24 * time.Hour)),
+		Resolves: time.Now().Add(24 * time.Hour),
+	}
+
+	newEstimate := NewEstimate{
+		Reason: "My weather app says it will rain.",
+		Probabilities: []NewProbability{
+			{
+				Value: 70,
+				Outcome: NewOutcome{
+					Text: "Yes",
+				},
+			},
+			{
+				Value: 30,
+				Outcome: NewOutcome{
+					Text: "No",
+				},
+			},
+		},
+	}
+
+	respCreate, err := CreateForecast(
+		context.Background(),
+		c,
+		newForecast,
+		newEstimate,
+	)
+	require.NoError(t, err)
+	assert.Equal(t, "Will it rain tomorrow?", respCreate.CreateForecast.Title)
+
+	respGetForecasts, err = GetForecasts(context.Background(), c)
+	require.NoError(t, err)
+	assert.Len(t, respGetForecasts.Forecasts, 5)
+}
+
+// TestUpdate_From_0_1_1_sqlite3_dump verifies that the sqlite3 dump (if the
+// tool is installed) only contains time zones of the form +00:00 and does not
+// contain the Go null time.
+func TestUpdate_From_0_1_1_sqlite3_dump(t *testing.T) {
+	_, err := exec.LookPath("sqlite3")
+	if err != nil {
+		t.Skip("'sqlite3' is not installed")
+	}
+
+	tDir, err := os.MkdirTemp("", t.Name()+"_")
+	require.NoError(t, err)
+	dbSrc := filepath.Join("testdata", "test_0.1.1.db")
 	dbPath := filepath.Join(tDir, "test.db")
 	t.Log("executing with DB", dbPath)
 	err = CopyFile(dbSrc, dbPath)
@@ -22,4 +142,16 @@ func TestUpdate_From_0_1_1(t *testing.T) {
 	resp, err := GetForecasts(context.Background(), c)
 	require.NoError(t, err)
 	require.Len(t, resp.Forecasts, 4)
+
+	cmd := exec.Command("sqlite3", dbPath, ".dump")
+	var cleosrvStdout, cleosrvStderr bytes.Buffer
+	cmd.Stdout = &cleosrvStdout
+	cmd.Stderr = &cleosrvStderr
+	err = cmd.Run()
+	require.NoError(t, err, cleosrvStderr.String())
+	tzRegex, _ := regexp.Compile(`[+-]\d\d:\d\d`)
+	for _, tz := range tzRegex.FindAllString(cleosrvStdout.String(), -1) {
+		assert.Equal(t, "+00:00", tz)
+	}
+	assert.NotContains(t, cleosrvStdout.String(), "0001-01-01 00:00:00+00:00")
 }
