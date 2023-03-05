ffc4663fdcd05fb4c1f11c7ddf63c66cf9f3fbc3
Omar Kohl <omarkohl@posteo.net>
2023-03-05 16:00:57 +0100

    test(e2e): add default cypress settings for manual execution
    Mage overrides these when executing 'mage e2etest' but it's useful when
    developing further e2e tests:
    
        cd e2etest
        npx cypress open

--- e2e_tests/cypress.config.ts
+++ e2e_tests/cypress.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  env: {
+    cleocPath: '../dist/cleoc_linux_amd64_v1/cleoc',
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'http://localhost:8080',
   },
 });
