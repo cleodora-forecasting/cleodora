51168db2431144953ac986d02a25b38e21ab5a2f
Omar Kohl <omarkohl@posteo.net>
2023-05-24 20:48:03 +0200

    test(frontend): extend ForecastDetails test a little

--- frontend/src/ForecastDetails.test.tsx
+++ frontend/src/ForecastDetails.test.tsx
@@ -9,7 +9,25 @@ test('the forecast is displayed', () => {
         created: undefined,
         description: "If it rains continuously for more than 30 minutes, that" +
             " counts as rain.",
-        estimates: [],
+        estimates: [
+            {
+                id: "E01",
+                created: "2022-10-30T17:05:00+01:00",
+                reason: "Just a hunch.",
+                brierScore: null,
+                probabilities: [
+                    {
+                        id: "P01",
+                        value: 20,
+                        outcome: {
+                            id: "O01",
+                            correct: false,
+                            text: "Yes",
+                        },
+                    },
+                ],
+            },
+        ],
         id: "",
         resolution: Resolution.Unresolved,
         resolves: undefined,
@@ -24,4 +42,6 @@ test('the forecast is displayed', () => {
 
     expect(screen.getByRole("heading", {name: "Will it rain tomorrow?"})).toBeInTheDocument();
     expect(screen.getByText(/more than 30 minutes/)).toBeInTheDocument();
+    expect(screen.getByText(/UNRESOLVED/)).toBeInTheDocument();
+    expect(screen.getByText(/Just a hunch./)).toBeInTheDocument();
 });
