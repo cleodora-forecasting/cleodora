8f78cd76565b713577b9cf44f7b2c876a4f6d1e7
Omar Kohl <omarkohl@posteo.net>
2024-01-07 20:32:37 +0100

    build: fix dates in demoDummyData script

--- scripts/demoDummyData.sh
+++ scripts/demoDummyData.sh
@@ -6,8 +6,8 @@ set -o pipefail
 ./dist/cleoc_linux_amd64_v1/cleoc \
     --url https://demo.cleodora.org \
     add forecast \
-    --title "Will Cleodora have 5 or more contributors by the end of 2023?" \
-    --resolves 2024-01-01T00:00:00+01:00 \
+    --title "Will Cleodora have 5 or more contributors by the end of 2024?" \
+    --resolves 2025-01-01T00:00:00+01:00 \
     --description "A contributor is any person who makes any change to the" \
         "Git repository (which includes cleosrv, cleoc, website etc.)." \
     --probability Yes=70 \
